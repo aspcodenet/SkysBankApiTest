c2c70688277f02c83c711b09edb556a81ad3ea33
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-02-09 10:16:51 +0100

    Add project files.

---
+++ SkysBankApiTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkysBankApiTest.Api;
+
+namespace SkysBankApiTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SweaWebServicePortTypeClient();
+            var days = client.getCalendarDays(new DateTime(2022, 1, 1), new DateTime(2022, 2, 2));
+            foreach (var day in days)
+            {
+                Console.WriteLine(day.bankday);
+            }
+        }
+    }
+}
